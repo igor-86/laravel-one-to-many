4c4b0d4de381213b5c51fd04b441cdecbac42b4b
Igor-86 <i.campoli86@gmail.com>
2023-01-16 21:00:18 +0100

    Add: Operazioni Crud - Create - Select Type

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -18,6 +18,16 @@
                         @enderror
                     </div>
                     <div class="form-group mb-3">
+                        <label for="type">Categoria</label>
+                        <select name="type_id" id="type" class="form-select">
+                            <option value="">Nessuna categoria</option>
+                            @foreach ($types as $type)
+                                <option value="{{ $type->id }}" @selected(old('type_id') == $type->id)>{{ $type->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group mb-3">
                         <label for="cover_image">Immagine</label>
                         <input type="file" name="cover_image" id="cover_image"
                             class="form-control @error('cover_image') is-invalid @enderror">
